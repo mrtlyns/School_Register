a5c3df72dfa7e09451fec069b2f7764087b29bb0
Yunus <Yunus@DESKTOP-E7HJ6O2>
2021-11-03 16:38:40 +0300

    Proje dosyası ekle.

---
+++ School_Register/Index.aspx.designer.cs
@@ -0,0 +1,17 @@
+﻿//------------------------------------------------------------------------------
+// <otomatik üretildi>
+//     Bu kod bir araç tarafından oluşturuldu.
+//
+//     Bu dosyada yapılacak değişiklikler yanlış davranışa neden olabilir ve
+//     kod tekrar üretildi. 
+// </otomatik üretildi>
+//------------------------------------------------------------------------------
+
+namespace School_Register.web
+{
+
+
+    public partial class Index
+    {
+    }
+}
